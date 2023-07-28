0af23087b8fe76ed38b4fa8bfc31aa399730ef4d
黄健锋 <huangjianfeng@seerbigdata.com>
2023-07-28 15:04:57 +0800

    feat: list2Tree - 增加root参数

--- packages/tree-utils/src/list2Tree.ts
+++ packages/tree-utils/src/list2Tree.ts
@@ -1,37 +1,36 @@
 export const list2Tree = <T>(
-    list: T[],
-    option: { key: keyof T; parentKey: keyof T },
-  ): (T & { children: T })[] => {
-    if (!(list instanceof Array)) {
-      throw new Error('List is not Array!');
+  list: T[],
+  option: { key: keyof T; parentKey: keyof T; root?: string },
+): (T & { children: T })[] => {
+  if (!(list instanceof Array)) {
+    throw new Error('List is not Array!');
+  }
+
+  const { key, parentKey, root } = option;
+  const tempList: any[] = JSON.parse(JSON.stringify(list));
+
+  const map: Record<string, any[]> = {};
+
+  tempList.forEach((item) => {
+    if (!map[item[parentKey]]) {
+      map[item[parentKey]] = [];
     }
-  
-    const { key, parentKey } = option;
-    const tempList: any[] = JSON.parse(JSON.stringify(list));
-  
-    const map: Record<string, any[]> = {};
-  
-    tempList.forEach((item) => {
-      if (!map[item[parentKey]]) {
-        map[item[parentKey]] = [];
-      }
-      map[item[parentKey]].push(item);
-    });
-  
-    return tempList.filter((item) => {
-      const childs = map[item[key]];
-      if (childs?.length > 0) {
-        if (!item.children) {
-          item.children = [];
-        }
-        item.children.push(...childs);
-      } else {
+    map[item[parentKey]].push(item);
+  });
+
+  return tempList.filter((item) => {
+    const childs = map[item[key]];
+    if (childs?.length > 0) {
+      if (!item.children) {
         item.children = [];
       }
-  
-      if (!item[parentKey]) {
-        return item;
-      }
-    });
-  };
-  
+      item.children.push(...childs);
+    } else {
+      item.children = [];
+    }
+
+    if (!item[parentKey] || item[parentKey] === root) {
+      return item;
+    }
+  });
+};
